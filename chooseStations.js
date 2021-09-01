95d54fbf0e30a7b3b016541e4dad473cad35e4cb
remained3 <sarahthebush@hotmail.com>
2021-09-01 09:17:46 +0000

    function adding suitable voting stations to an array

---
+++ chooseStations.js
@@ -0,0 +1,19 @@
+function chooseStations(stations) {
+  const goodStations = [];
+  for (const station of stations) {
+    const capacity = station[1];
+    const venueType = station[2];
+
+    if ((capacity >= 20 && venueType === 'school') || (capacity >= 20 && venueType === 'community centre')) {
+      goodStations.push(station[0])
+    }
+  }
+  return goodStations
+}
+const stations = [
+  ['Big Bear Donair', 10, 'restaurant'],
+  ['Bright Lights Elementary', 50, 'school'],
+  ['Moose Mountain Community Centre', 45, 'community centre']
+];
+
+chooseStations(stations);
